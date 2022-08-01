5d583b3284da0913d31076a28dd1123082e33397
Kenny Robert Gomes Da Silva <2006315@aluno.univesp.br>
2022-08-01 20:35:30 -0300

    Initial commit

---
+++ aula 03/olaMundo.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro Programa em PHP</title>
+    <style>
+        body{
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        h1{
+            color: darkblue;
+        }
+        p { 
+            color: darkblue;
+        }
+    </style>
+</head>
+<body>
+    <h1>Primeiro programa em PHP</h1>
+    <?php
+        echo "<p>Olá, mundo!</p>"; 
+    ?>
+</body>
+</html>
