c8b9a622a3c8e4002b9020114ef1c7e42a557b1c
Kenny Robert Gomes Da Silva <2006315@aluno.univesp.br>
2022-08-02 21:01:08 -0300

    aula 04

---
+++ aula 04/tipos.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos de dados</title>
+</head>
+<body>
+    <?php 
+        $idade = 40;
+        $nome = "José";
+        $filhos = 3;
+        $esposa = "Josefa";
+        echo "<p>$nome tem $idade anos, e tem $filhos filhos </p>";
+        echo "<p>A esposa de $nome se chama $esposa.</p>"
+    ?>
+</body>
+</html>
